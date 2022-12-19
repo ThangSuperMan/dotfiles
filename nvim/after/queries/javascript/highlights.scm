18098c397d33200759ed759bb672778c62894c73
thangsuperman <thangjenny2002@gmail.com>
2022-12-19 18:29:02 +0700

    Switch the vimscript nvim to the lua nvim, haha

--- nvim/after/queries/javascript/highlights.scm
+++
@@ -1,6 +0,0 @@
-("function" @keyword (#set! conceal  "ƒ"))
-("import" @keyword (#set! conceal  "⟻"))
-("export" @keyword (#set! conceal  "⟼"))
-("=>" @keyword (#set! conceal  "⇒"))
-; (((return_statement) @field (#eq? @field "return" )) (#set! conceal ">"))
-
--- nvim/after/queries/typescript/highlights.scm
+++
@@ -1,4 +0,0 @@
-("function" @keyword (#set! conceal  "ƒ"))
-("import" @keyword (#set! conceal  "⟻"))
-("export" @keyword (#set! conceal  "⟼"))
-("=>" @keyword (#set! conceal  "⇒"))
